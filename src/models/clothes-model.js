f961f794d5a8622e26962ecd878616118e64a417
Clement Buchanan <clement.buchanan@gmail.com>
2021-03-04 23:03:04 -0800

    done and done

--- src/models/clothes-model.js
+++ src/models/clothes-model.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 // Create a new mongoose schema with a set of properties and constraints
 const clothesSchema = mongoose.Schema({
     brand: { type: String, required: true },   // This must be a string and provided (req)
-    color: { type: Number, required: true },
+    color: { type: String, required: true },
     type: { type: String, uppercase: true, enum: ['SHIRT', 'PANTS', 'SHORTS'] }  //enum means the data must be one of the enumerated values
 });
 
