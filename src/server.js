38137a5cdcd38ecf27c923187aefcd270a9943f5
Clement Buchanan <clement.buchanan@gmail.com>
2021-03-04 22:47:35 -0800

    added clothesRoutes

--- src/server.js
+++ src/server.js
@@ -3,8 +3,10 @@
 const express = require('express');
 const app = express();
 const foodRoutes = require('./routes/food');
+const clothesRoutes = request('/routes/clothes');
 app.use(express.json());
 app.use(foodRoutes);
+app.use(clothesRoutes);
 
 const notFound = require('./error-handlers/404.js');
 const errors = require('./error-handlers/500.js');
